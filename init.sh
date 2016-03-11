ceec0be3be7d6a9755d5e3ad6b6654b94a0cab50
Black Borland <blackbek@icloud.com>
2016-03-11 14:04:15 +0300

    add config and init.sh 2

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ﻿passwd root ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ﻿ ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx start
