a58b77faabd645510fbe1268da25e7387c0a0e3e
BlackBorland <blackbek@icloud.com>
2016-03-17 17:23:14 +0400

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,17 +1,8 @@
-from cgi import parse_qs
-
-def application(environ, start_response):
-
-  query = cgi.parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
-  body = []
-  for key, values in query.items():
-    for item in values:
-      body.append(key + "=" + item + "\r\n")
-
-  status = '200 OK'
-  headers = [
-   ('Content-Type', 'text/plain')
-  ]
-
-  start_response(status, headers)
-  return body
+#!/usr/bin/python
+def app(environ, start_response):
+    data = ''
+    for line in environ["QUERY_STRING"].split("&"):
+        data = data+line+"\n"
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [data]
+    
